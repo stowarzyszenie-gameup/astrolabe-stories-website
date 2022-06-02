7fdd75ece1828dbef8a3d2a13e189601578d029e
Michał Hawełka <m.hawelka@gmail.com>
2022-06-02 21:35:53 +0200

    Animated background

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -9,9 +9,12 @@ interface IProps {
 const Layout = ({ children }: IProps) => {
   return (
     <div className="flex flex-col lg:flex-row font-primary">
+      <div className="stars fixed top-0 left-0 right-0 bottom-0 w-full h-full block bg-stars bg-repeat bg-center-top -z-50"></div>
+      <div className="twinkling fixed top-0 left-0 right-0 bottom-0 w-full h-full block bg-twinkling bg-repeat bg-center-top -z-40 animate-twink"></div>
+      <div className="clouds fixed top-0 left-0 right-0 bottom-0 w-full h-full block bg-clouds bg-repeat bg-center-top -z-30 animate-cloud opacity-40"></div>
       <Navbar />
       <div className="w-full">
-        <main className="bg-black">{children}</main>
+        <main>{children}</main>
         <Footer />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         primary: "#9B7340",
         secondary: "rgb(16, 37, 66)",
       },
+      backgroundImage: {
+        stars: "url('/img/stars.png'), linear-gradient(to right, #000, #000)",
+        twinkling: "url('/img/twinkling.png')",
+        clouds: "url('/img/clouds.png')",
+      },
+      animation: {
+        twink: "move-twinks 200s linear infinite",
+        cloud: "move-clouds 200s linear infinite",
+      },
     },
   },
   plugins: [],
