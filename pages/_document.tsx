75132320d98a4cbcde72957b721c6e8039dd9b9b
Michał Hawełka <m.hawelka@gmail.com>
2022-06-03 18:35:03 +0200

    HTML lang update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class AstrolabeDocument extends Document {
   render() {
     return (
-      <Html lang="pl" className="scroll-smooth">
+      <Html lang="en" className="scroll-smooth">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;1,100&display=swap"
